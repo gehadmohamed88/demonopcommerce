ea8cff21edddff9e7aed0743c68dd8d17f904428
Gehad <gehad_mohamed_8@hotmail.com>
2023-04-17 01:25:40 +0000

    First commit

--- src/test/java/org/example/pages/P01_register.java
+++ src/test/java/org/example/pages/P01_register.java
@@ -17,6 +17,84 @@ public class P01_register
 
 
 
+    public WebElement kind()
+    {
+
+        return Hooks.driver.findElement(By.id("gender-male"));
+
+    }
+
+    public WebElement firstname()
+    {
+
+
+        return Hooks.driver.findElement(By.id("FirstName"));
+
+
+    }
+
+    public WebElement lastname()
+
+    {
+
+        return Hooks.driver.findElement(By.id("LastName"));
+
+    }
+
+
+    public WebElement dateofbirthday()
+    {
+
+        return Hooks.driver.findElement(By.cssSelector("select[name=\"DateOfBirthDay\"] option[value=\"8\"]"));
+
+    }
+
+
+    public WebElement dateofbirthmonth()
+    {
+
+        return Hooks.driver.findElement(By.cssSelector("select[name=\"DateOfBirthMonth\"] option[value=\"8\"]"));
+
+    }
+
+
+
+    public WebElement dateofbirthyears()
+    {
+
+        return Hooks.driver.findElement(By.cssSelector("select[name=\"DateOfBirthYear\"] option[value=\"1994\"]"));
+
+    }
+
+
+    public WebElement enteremail()
+    {
+
+        return Hooks.driver.findElement(By.id("Email"));
+
+    }
+
+
+    public WebElement enterpassword()
+    {
+
+        return Hooks.driver.findElement(By.id("Password"));
+
+    }
+
+    public WebElement enterpasswordconfirmation()
+    {
+
+        return Hooks.driver.findElement(By.id("ConfirmPassword"));
+
+    }
+
+
+
+
+
+
+
 
 
 }
--- src/test/java/org/example/stepDefs/D01_registerStepDef.java
+++ src/test/java/org/example/stepDefs/D01_registerStepDef.java
@@ -1,9 +1,12 @@
 package org.example.stepDefs;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 import org.example.pages.P01_register;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class D01_registerStepDef {
 
@@ -16,11 +19,62 @@ public class D01_registerStepDef {
 
         register.registerlink().click();
 
+        //WebElement registerBtn = Hooks.driver.findElement(By.cssSelector("a[href=\"/register?returnUrl=%2F\"]"));
+        //registerBtn.click();
+
+    }
+
+    @When("user select gender type")
+    public void userSelectGenderType()
+    {
+
+        register.kind().click();
+
+    }
 
 
-        WebElement registerBtn = Hooks.driver.findElement(By.cssSelector("a[href=\"/register?returnUrl=%2F\"]"));
-        registerBtn.click();
+    @And("user enter first name {string} and last name {string}")
+    public void userEnterFirstNameAndLastName(String arg0, String arg1)
+    {
+        register.firstname().sendKeys("automation");
 
+        register.lastname().sendKeys("tester");
+
+
+    }
+
+
+    @And("user enter date of birth")
+    public void userEnterDateOfBirth()
+    {
+
+        register.dateofbirthday().click();
+
+        register.dateofbirthmonth().click();
+
+        register.dateofbirthyears().click();
+
+
+    }
+
+
+    @And("user enter email {string} field")
+    public void userEnterEmailField(String arg0)
+    {
+
+
+        register.enteremail().sendKeys("test@example.com");
+
+    }
+
+
+    @And("user fills Password fields {string} {string}")
+    public void userFillsPasswordFields(String arg0, String arg1)
+    {
+
+        register.enterpassword().sendKeys("P@ssw0rd");
+
+        register.enterpasswordconfirmation().sendKeys("P@ssw0rd");
 
 
     }
